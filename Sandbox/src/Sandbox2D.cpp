62756fbaf6068c96cab14baff253c8d1286be98e
SaNeOr <sane0r@qq.com>
2020-03-07 04:17:51 +0800

    profiling

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -7,6 +7,43 @@
 
 #include "Platform/OpenGL/OpenGLShader.h"
 
+#include <chrono>
+
+template<typename Fn>
+class Timer {
+public:
+	Timer(const char* name, Fn&& func)
+		:m_Name(name), m_Func(func), m_Stopped(false) {
+		m_StartTimepoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer() {
+		if (!m_Stopped)
+			Stop();
+	}
+	void Stop() {
+		auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+		m_Stopped = true;
+		float duration = (end - start) * 0.001f;
+		std::cout << m_Name << ": Duration: " << duration <<"ms" << std::endl;
+		m_Func({ m_Name, duration });
+	}
+
+private:
+	const char* m_Name;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+	bool m_Stopped;
+
+	Fn m_Func;
+
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name,[&](ProfileResult profileResult) {m_ProfileResults.push_back(profileResult); })
+
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true)
 {
@@ -57,7 +94,14 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Niking2D::Timestep ts)
 {
-	m_CameraController.OnUpdate(ts);
+
+	PROFILE_SCOPE("Sandbox2D::OnUpdate");
+	{
+		PROFILE_SCOPE("m_CameraController::OnUpdate");
+		m_CameraController.OnUpdate(ts);
+	}
+
+
 
 	Niking2D::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 	Niking2D::RenderCommand::Clear();
@@ -83,6 +127,15 @@ void Sandbox2D::OnImGuiRender()
 	ImGui::Begin("Settings");
 	//ImGui::Text("Hello World");
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+
+	for (auto& result : m_ProfileResults) {  
+		char lable[50];
+		strcpy(lable, "%.3fms ");
+		strcat(lable, result.Name);
+		ImGui::Text(lable, result.Time);
+	}
+	m_ProfileResults.clear();
+
 	ImGui::End();
 }
 
